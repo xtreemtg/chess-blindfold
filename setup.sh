4873436416a96cb78f16932b86c241b389052bb6
xtreemtg <88gigabites@gmail.com>
2024-07-18 23:12:43 +0300

    add deploy files for github pages and github actions temp removing tests

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@
 set -e
 
 # Installing packages
-npm install
+#npm install
 
 # Downloading the stockfish binary. 
 mkdir -p serve_content/shared
 wget -O serve_content/shared/stockfish.js "https://github.com/exoticorn/stockfish-js/releases/download/sf_5_js/stockfish.js"
 
 # Running tests
-npm run test
+#npm run test
 
 # Building the library if the tests succeed
-npm run build
+#npm run build
 
